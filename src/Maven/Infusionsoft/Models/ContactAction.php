c472ba8a648d161821c047981a8df9ef91d60044
Dustin Lunt <dustin@marketingmavens.com>
2014-06-03 16:46:43 -0700

    Added the method on ContactAction addNote

--- src/Maven/Infusionsoft/Models/ContactAction.php
+++ src/Maven/Infusionsoft/Models/ContactAction.php
@@ -6,5 +6,35 @@ class ContactAction extends BaseModel
 
     public static $table = 'ContactAction';
 
+
+    /**
+     *
+     * Adds a note on the contact record
+     * @param        $contactId
+     * @param string $title
+     * @param string $description
+     * @param string $actionType
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function addNote($contactId, $title = 'Note', $description = '', $actionType = 'Other')
+    {
+        $note = [
+            'ContactId' => $contactId,
+            'ActionDescription' => $title,
+            'CreationNotes' =>  $description,
+            'ActionType' => $actionType
+        ];
+        $noteId = $this->SDK->dsAdd(self::$table, $note);
+        if(is_int($noteId)) {
+            $note['Id'] = $noteId;
+
+            return $note;
+        }
+
+        throw new \Exception('Error Creating Note');
+    }
+
 }
 
