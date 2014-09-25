e8e229a4f71fbb27d7b72735c2e41bff2e753947
Dustin Lunt <dustin@marketingmavens.com>
2014-09-25 16:43:33 -0700

    =Updated the subscription plan list builder to account for Subscriptions that don't have a plan name

--- src/Maven/Infusionsoft/Models/SubscriptionPlan.php
+++ src/Maven/Infusionsoft/Models/SubscriptionPlan.php
@@ -1,35 +1,52 @@
 <?php
 namespace Maven\Infusionsoft\Models;
 
-class SubscriptionPlan extends BaseModel
-{
+class SubscriptionPlan extends BaseModel {
 
     public static $table = 'SubscriptionPlan';
 
-	public $subscriptionCycles = [
-		'1' => 'Year',
-		'2' => 'Month',
-		'3' => 'Week',
-		'6' => 'Day',
-	];
+    public $subscriptionCycles = [
+        '1' => 'Year',
+        '2' => 'Month',
+        '3' => 'Week',
+        '6' => 'Day',
+    ];
 
-	public function getAllAsList()
-	{
-		$subscriptions = $this->get(['Id' => '%']);
-		if (!is_array($subscriptions)) throw new \Exception('Unexpected response when attempting to find all subscriptions: '.$subscriptions);
-		$products = $this->get(['Id' => '%'], 'Product', ['Id', 'ProductName']);
-		if (!is_array($products)) throw new \Exception('Unexpected response when attempting to find all products: '.$products);
-		$products = $this->getIdArray($products);
-		$subscriptonList = [];
-		foreach ($subscriptions as $subscription) {
-			$existingProduct = $products[$subscription['ProductId']];
-			$selectedCycle = strtolower($this->subscriptionCycles[$subscription['Cycle']]);
-			$lengthTag = $subscription['Frequency'] == 1 ? $selectedCycle : $subscription['Frequency'].' '.$selectedCycle.'s';
-			$subscriptonList[$subscription['Id']] = "{$existingProduct['ProductName']} - $".number_format($subscription['PlanPrice'], 2, '.', '')." billed every $lengthTag";
-		}
+    public function getAllAsList()
+    {
+        $subscriptions = $this->get(['Id' => '%']);
+        if ( !is_array($subscriptions) ) throw new \Exception('Unexpected response when attempting to find all subscriptions: ' . $subscriptions);
+        $products = $this->get(['Id' => '%'], 'Product', ['Id', 'ProductName']);
+        if ( !is_array($products) ) throw new \Exception('Unexpected response when attempting to find all products: ' . $products);
+        $products = $this->getIdArray($products);
+        $subscriptionList = [];
+        foreach ( $subscriptions as $subscription )
+        {
+            $subscriptionList = $this->subscriptionName($products, $subscription, $subscriptionList);
+        }
 
-		return $subscriptonList;
-	}
+        return $subscriptionList;
+    }
+
+    /**
+     * @param $products
+     * @param $subscription
+     * @param $subscriptionList
+     *
+     * @return mixed
+     */
+    protected function subscriptionName($products, $subscription, $subscriptionList)
+    {
+        $existingProduct = $products[$subscription['ProductId']];
+        $selectedCycle = strtolower($this->subscriptionCycles[$subscription['Cycle']]);
+        $lengthTag = $subscription['Frequency'] == 1 ? $selectedCycle : $subscription['Frequency'] . ' ' . $selectedCycle . 's';
+        $subscriptionList[$subscription['Id']] = "{$existingProduct['ProductName']} - $";
+        $planPrice = isset($subscription['PlanPrice']) ? $subscription['PlanPrice'] : 0;
+        $subscriptionList[$subscription['Id']] .= number_format($planPrice, 2, '.', '');
+        $subscriptionList[$subscription['Id']] .= " billed every $lengthTag";
+
+        return $subscriptionList;
+    }
 
 }
 
