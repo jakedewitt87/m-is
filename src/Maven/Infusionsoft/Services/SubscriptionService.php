5c6ee8b906c7511c7ee2ba66f0ecc25f38abed7e
Dustin Lunt <dustin@marketingmavens.com>
2015-06-29 08:45:22 -0700

    Updated the add and charge subscription to allow for deleting the invoice and subscription if it failes

--- src/Maven/Infusionsoft/Services/SubscriptionService.php
+++ src/Maven/Infusionsoft/Services/SubscriptionService.php
@@ -1,8 +1,28 @@
 <?php namespace Maven\Infusionsoft\Services;
 
+use Exception;
+
+/**
+ * Class SubscriptionService
+ *
+ * @package Maven\Infusionsoft\Services
+ */
 class SubscriptionService extends BaseService {
 
-    public function addAndChargeSubscription($contactId, $subscriptionPlanId, $subscriptionPrice, $creditCardId, $merchantAccountId, $orderNotes = '', $affiliateId = 0)
+    /**
+     * @param        $contactId
+     * @param        $subscriptionPlanId
+     * @param        $subscriptionPrice
+     * @param        $creditCardId
+     * @param        $merchantAccountId
+     * @param string $orderNotes
+     * @param int    $affiliateId
+     * @param bool   $deleteFailedInvoice
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public function addAndChargeSubscription($contactId, $subscriptionPlanId, $subscriptionPrice, $creditCardId, $merchantAccountId, $orderNotes = '', $affiliateId = 0, $deleteFailedInvoice = false)
     {
         $subscriptionId = $this->SDK->addRecurringAdv($contactId,
             true,
@@ -15,18 +35,54 @@ class SubscriptionService extends BaseService {
             $affiliateId,
             0
         );
-        if ( ! is_numeric($subscriptionId) || $subscriptionId <= 0 ) throw new \Exception('Unable to create new subscription: ' . $subscriptionId, 400);
+        if ( ! is_numeric($subscriptionId) || $subscriptionId <= 0 ) throw new Exception('Unable to create new subscription: ' . $subscriptionId, 400);
 
         $invoiceId = $this->SDK->recurringInvoice($subscriptionId);
-        if ( ! is_numeric($invoiceId) || $invoiceId <= 0 ) throw new \Exception('Unable to create new invoice for subscription: ' . $invoiceId, 400);
+        if ( ! is_numeric($invoiceId) || $invoiceId <= 0 ) throw new Exception('Unable to create new invoice for subscription: ' . $invoiceId, 400);
 
         $chargeResponse = $this->SDK->chargeInvoice($invoiceId, $orderNotes, intval($creditCardId), intval($merchantAccountId), false);
-        if ( ! $chargeResponse || ! is_array($chargeResponse) ) throw new \Exception('Unexpected response when attempting to charge invoice: ' . $chargeResponse);
-        if ( $chargeResponse['Successful'] != true ) throw new \Exception('Unable to charge payment on subscription: ' . $chargeResponse['Message']);
+        if ( ! $chargeResponse || ! is_array($chargeResponse) )
+        {
+            if ($deleteFailedInvoice) $this->deleteSubscriptionAndInvoice($subscriptionId, $invoiceId);
+
+            throw new Exception('Unexpected response when attempting to charge invoice: ' . $chargeResponse);
+        }
+        if ( $chargeResponse['Successful'] != true )
+        {
+            if ($deleteFailedInvoice) $this->deleteSubscriptionAndInvoice($subscriptionId, $invoiceId);
+
+            throw new Exception('Unable to charge payment on subscription: ' . $chargeResponse['Message']);
+        }
 
         return $subscriptionId;
     }
 
+    /**
+     * @param null $subscriptionId
+     * @param null $invoiceId
+     *
+     * @return bool
+     */
+    public function deleteSubscriptionAndInvoice($subscriptionId = null, $invoiceId = null)
+    {
+        if ( $subscriptionId ) $this->SDK->deleteSubscription($subscriptionId);
+
+        if ( $invoiceId ) $this->SDK->deleteInvoice($invoiceId);
+
+        return true;
+    }
+
+    /**
+     * @param     $contactId
+     * @param     $subscriptionPlanId
+     * @param     $subscriptionPrice
+     * @param     $merchantAccountId
+     * @param     $creditCardId
+     * @param int $affiliateId
+     *
+     * @return mixed
+     * @throws Exception
+     */
     public function addSubscriptionWithInvoice($contactId, $subscriptionPlanId, $subscriptionPrice, $merchantAccountId, $creditCardId, $affiliateId = 0)
     {
         $subscriptionId = $this->SDK->addRecurringAdv($contactId,
@@ -40,10 +96,10 @@ class SubscriptionService extends BaseService {
             $affiliateId,
             0
         );
-        if ( ! is_numeric($subscriptionId) || $subscriptionId <= 0 ) throw new \Exception('Unable to create new subscription: ' . $subscriptionId, 400);
+        if ( ! is_numeric($subscriptionId) || $subscriptionId <= 0 ) throw new Exception('Unable to create new subscription: ' . $subscriptionId, 400);
 
         $invoiceId = $this->SDK->recurringInvoice($subscriptionId);
-        if ( ! is_numeric($invoiceId) || $invoiceId <= 0 ) throw new \Exception('Unable to create new invoice for subscription: ' . $invoiceId, 400);
+        if ( ! is_numeric($invoiceId) || $invoiceId <= 0 ) throw new Exception('Unable to create new invoice for subscription: ' . $invoiceId, 400);
 
         return $invoiceId;
     }
